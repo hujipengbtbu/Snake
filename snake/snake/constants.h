502f0adc2c69cde98704fd75c554fbb8dcfdc93c
hujipengbtbu <1057912859@qq.com>
2017-09-10 18:41:55 +0800

    Creat Snake

---
+++ snake/snake/constants.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int TILE_SIZE = 10;
+const int TILE_SIZE1 = 30;
+
+enum GameObjectsData {
+    GD_Type
+};
+
+enum GameObjectTypes {
+    GO_Food,
+    GO_Wall
+};
+
+#endif // CONSTANTS_H
